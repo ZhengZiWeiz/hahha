4c27947bb03fbca8949c992bd39cf11ae4dab4fa
zzw <250502>
2024-11-17 19:56:48 +0800

    frist commit

---
+++ src/main/java/com/atguigu/git.java
@@ -0,0 +1,16 @@
+package com.atguigu;
+
+/**
+ * ClassName: git
+ * Package: com.atguigu
+ * Description:
+ *
+ * @Author:zzw
+ * @Create:2024/11/17 - 19:53
+ */
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
